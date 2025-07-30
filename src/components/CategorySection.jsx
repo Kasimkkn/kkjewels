7cd1a53ae2a1d4ae25689503eb6fd95b91238861
[dyad] <git@dyad.sh>
2025-07-30 08:40:44 +0530

    [dyad] Implemented carousel in CategorySection - wrote 1 file(s)

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
 import starImage from '../assets/img/star.png';
 import p1 from '../assets/img/p1.jpg';
 import p2 from '../assets/img/p2.jpg';
 import p3 from '../assets/img/p3.jpg';
 import p4 from '../assets/img/p4.jpg';
 import p5 from '../assets/img/p5.jpg';
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselPrevious,
+    CarouselNext,
+} from "./ui/carousel";
 
 const imageArray = [p4, p2, p1, p3, p5];
 
@@ -23,59 +29,31 @@ const CategorySection = () => {
                 <span className="mutligrateintAnimatedText font-decorative lg:text-7xl text-5xl">View</span>
             </h1>
             <div className="relative mt-20 md:mt-48 flex w-full max-md:flex-col">
-                <button
-                    className="px-6 py-2 rounded-full z-50 bg-black text-white absolute md:top-0 -top-8 md:left-1/4 left-12 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-                    <BsArrowLeft color='white' className='text-xl' />
-                </button>
-                <button
-                    className="px-6 py-2 w-max rounded-full z-50 bg-black text-white absolute md:top-12 -top-8 md:left-1/4 right-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-                    <BsArrowRight color='white' className='text-xl' />
-                </button>
-                <div className="md:relative max-md:whitespace-nowrap max-md:overflow-x-auto top-1/2 left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 flex justify-center items-center">
-                    {imageArray.map((image, index) => {
-                        let rotationClass = '';
-                        let positionClass = '';
-                        let zIndex = '';
-
-                        // Apply rotation and positioning based on the index
-                        switch (index) {
-                            case 0:
-                                rotationClass = '-left-20 md:rotate-left';
-                                zIndex = 'z-10';
-                                break;
-                            case 1:
-                                rotationClass = '-left-10 md:rotate-left';
-                                zIndex = 'z-20';
-                                break;
-                            case 2:
-                                rotationClass = 'md:relative -top-4';
-                                zIndex = 'z-30';
-                                break;
-                            case 3:
-                                rotationClass = '-right-10 md:rotate-right';
-                                zIndex = 'z-20';
-                                break;
-                            case 4:
-                                rotationClass = '-right-20 md:rotate-right';
-                                zIndex = 'z-10';
-                                break;
-                            default:
-                                break;
-                        }
-
-                        return (
-                            <div
-                                key={index}
-                                className={`md:absolute overflow-hidden md:w-96 w-full h-80 shadow-lg ${rotationClass} ${zIndex}`}>
-                                <img
-                                    src={image}
-                                    alt={`Category Image ${index + 1}`}
-                                    className="w-full h-full object-cover rounded-lg"
-                                />
-                            </div>
-                        );
-                    })}
-                </div>
+                <Carousel
+                    opts={{
+                        align: "start",
+                        loop: true,
+                    }}
+                    className="w-full relative"
+                >
+                    <CarouselContent className="-ml-1">
+                        {imageArray.map((image, index) => (
+                            <CarouselItem key={index} className="pl-1 md:basis-1/3 lg:basis-1/4">
+                                <div className="p-1">
+                                    <div className="overflow-hidden w-full h-80 shadow-lg">
+                                        <img
+                                            src={image}
+                                            alt={`Category Image ${index + 1}`}
+                                            className="w-full h-full object-cover rounded-lg"
+                                        />
+                                    </div>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2 bg-black text-white border-none hover:bg-gray-800" />
+                    <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 bg-black text-white border-none hover:bg-gray-800" />
+                </Carousel>
             </div>
             <div className="bg-black w-full z-40 h-40 flex justify-center items-center gap-20 whitespace-nowrap overflow-x-scroll md:absolute bottom-0">
                 <h3 className="italic  md:text-base text-sm text-gray-400 uppercase">RINGS</h3>
